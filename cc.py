2e70105dea40dcfd542a51258b911a40c3cfcbf3
Mr robot <30386159+edmark21@users.noreply.github.com>
2019-09-23 14:12:08 +0800

    Update cc.py

--- cc.py
+++ cc.py
@@ -10,63 +10,67 @@ _________                    .___.__  __    _________                  .___
 
         '''
 print(logo)
-def validator(n):
-
-    validatelist=[]
-
-    for i in n:
-        validatelist.append(int(i))
 
 
-    for i in range(0,len(n),2):
+"""
+Python script to check validity of credit card numbers
+Author : https://www.pythoncircle.Com
+"""
+
+import sys
 
 
-        validatelist[i] = validatelist[i]*2
+def usage():
+    msg = """
+        
+        usage:
+        python3 credit_card_validator credit_card_number
+        
+        example:
+        python3 credit_card_validator 34678253793
+        
+    """
+    print(msg)
 
-        if validatelist[i] >= 10:
 
-            validatelist[i] = validatelist[i]//10 + validatelist[i]%10
+def get_cc_number():
+    if len(sys.argv) < 2:
+        usage()
+        sys.exit(1)
+
+    return sys.argv[1]
 
 
-    if sum(validatelist)%10 == 0:
-        print('This a valid credit card') 
-
+def sum_digits(digit):
+    if digit < 10:
+        return digit
     else:
-        print('This is not valid credit card')
+        sum = (digit % 10) + (digit // 10)
+        return sum
 
-def cardnumber():
 
-    result=''
-    while True:
-        try:
-            result = input('Please enter the 16 digit credit card number : ')
-
-            if not (len(result) == 16) or not type(int(result) == int) :
-                raise Exception
-
-        except Exception:    
-            print('That is not a proper credit card number. \nMake sure you are entering digits not characters and all the 16 digits.')
-            continue
-
+def validate(cc_num):
+    # reverse the credit card number
+    cc_num = cc_num[::-1]
+    # convert to integer
+    cc_num = [int(x) for x in cc_num]
+    # double every second digit
+    doubled_second_digit_list = list()
+    digits = list(enumerate(cc_num, start=1))
+    for index, digit in digits:
+        if index % 2 == 0:
+            doubled_second_digit_list.append(digit * 2)
         else:
-            break
+            doubled_second_digit_list.append(digit)
+
+    # add the digits if any number is more than 9
+    doubled_second_digit_list = [sum_digits(x) for x in doubled_second_digit_list]
+    # sum all digits
+    sum_of_digits = sum(doubled_second_digit_list)
+    # return True or False
+    return sum_of_digits % 10 == 0
+     
 
 
-    return result
-
-def goagain():
-    return input('Do you want to check again? (Yes/No) : ').lower()[0] == 'y'
-
-def main():
-
-    while True:
-
-        result = cardnumber()
-        validator(result)
-
-
-        if not goagain():
-            break
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    print(validate(get_cc_number()))
